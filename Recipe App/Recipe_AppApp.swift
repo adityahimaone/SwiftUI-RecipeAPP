508c020edcd60c6c0ad35e949e2ae23d6019c0fa
adityahimaone <adityahimaone@gmail.com>
2024-02-25 16:31:45 +0700

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Aditya Himawan on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
